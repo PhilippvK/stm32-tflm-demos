cad150ffcdc7bb34e401923aecdd5e24bb8f2b38
Alex Hoffman <alxhoff@gmail.com>
2020-01-24 15:08:57 +0100

    Example with touchscreen and display builds, links, flashes and runs

---
+++ STM3240G-EVAL-Cmake/build/CMakeFiles/STM32F40G_Display.elf.bin.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STM32F40G_Display.elf.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/STM32F40G_Display.elf.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ STM3240G-EVAL-Cmake/build/CMakeFiles/STM32F40G_Display.elf.hex.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STM32F40G_Display.elf.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/STM32F40G_Display.elf.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
