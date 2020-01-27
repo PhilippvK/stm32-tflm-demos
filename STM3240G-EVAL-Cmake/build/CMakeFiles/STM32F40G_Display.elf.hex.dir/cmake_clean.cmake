7a44b514e4c88b4c58ab4b2a44c7a80d7e27939e
Alex Hoffman <alxhoff@gmail.com>
2020-01-27 11:44:00 +0100

    Gitignore

--- STM3240G-EVAL-Cmake/build/CMakeFiles/STM32F40G_Display.elf.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/STM32F40G_Display.elf.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/STM32F40G_Display.elf.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
